c07038548ae229247b9f27e479ec317b318589bd
mr-manson <60350677+mr-manson@users.noreply.github.com>
2020-03-13 08:37:11 +0300

    Add files via upload

---
+++ jd1-les11/src/by/htp/les11/obj02/Test2Main.java
@@ -0,0 +1,17 @@
+package by.htp.les11.obj02;
+
+public class Test2Main {
+
+	public static void main(String[] args) {
+		Test2 t = new Test2();
+		
+		t.print();
+		
+		t.setA(7);
+		t.setB(5);
+		
+		t.print();
+
+	}
+
+}
